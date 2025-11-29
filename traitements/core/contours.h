3fa27280b8a5f2285cc3a82e2d5df3d3aa1f5ef3
Hinsou-Pirkloss <djakbararoger@gmail.com>
2025-11-29 15:32:53 +0100

    Update contours.h

--- traitements/core/contours.h
+++ traitements/core/contours.h
@@ -1,4 +1,18 @@
 #ifndef CONTOURS_H
 #define CONTOURS_H
 
+#include <opencv2/opencv.hpp>
+
+// Paramètres pour la détection de contours (Canny)
+struct ParametresContours
+{
+    int seuilBas = 50;      // seuil inférieur
+    int seuilHaut = 150;    // seuil supérieur
+};
+
+// Fonction cœur de la détection de contours
+cv::Mat appliquerContoursCore(const cv::Mat &entree, const ParametresContours &params);
+
 #endif // CONTOURS_H
+
+
