3d73de2533236116c1a697773d76a3bd95e4c119
Hinsou-Pirkloss <djakbararoger@gmail.com>
2025-11-29 15:40:59 +0100

    adaptateur_contours.h

--- traitements/adaptateurs_qt/adaptateur_contours.h
+++ traitements/adaptateurs_qt/adaptateur_contours.h
@@ -1,4 +1,26 @@
 #ifndef ADAPTATEUR_CONTOURS_H
 #define ADAPTATEUR_CONTOURS_H
 
+
+#include "contours.h"
+#include <opencv2/opencv.hpp>
+
+// Adaptateur pour l'intégration dans le moteur générique
+class AdaptateurContours
+{
+public:
+    AdaptateurContours();
+
+    // Définit les paramètres
+    void setParametres(int seuilBas, int seuilHaut);
+
+    // Applique le traitement sur une image existante
+    void appliquer(cv::Mat &image);
+
+private:
+    ParametresContours params_;
+};
+
 #endif // ADAPTATEUR_CONTOURS_H
+
+
